b95287fe5e1038d875fdf51c6ce7688040652485
Nikolay <hellsing0072007@yandex.ru>
2022-05-13 16:58:49 +0300

    adaptive index page

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,18 @@ $(function(){
 		}else{
 			$('.header').removeClass('fixed');
 		}
-	})
+	});
+
+	$('.header-menu__mobile').click(function(e){
+		e.preventDefault();
+		$(this).toggleClass('open');
+		$('.header-menu__wrapper').toggleClass('open');
+	});
+
+	$('.footer__title').click(function(e){
+		e.preventDefault();
+		$(this).toggleClass('open');
+	});
+
 });
 
